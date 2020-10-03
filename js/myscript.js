cedde37e3636692503e2bd875d3d548d0752f19f
Belyse Uwambayinema <ubelyse1@gmail.com>
2020-10-03 21:13:26 +0200

    created a function with different variable to store our weekdays,male and female ghanians names

---
+++ js/myscript.js
@@ -0,0 +1,6 @@
+function mybirthday(){
+
+    var weekdays = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+    var maleName =["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
+    var femaleName = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
+}
