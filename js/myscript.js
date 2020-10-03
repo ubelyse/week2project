bb94f44867971dc04d6a05669197cb8a6bf9e1f5
Belyse Uwambayinema <ubelyse1@gmail.com>
2020-10-03 21:28:30 +0200

    created date variable that will store our entry of date of birth

--- js/myscript.js
+++ js/myscript.js
@@ -3,4 +3,8 @@ function mybirthday(){
     var weekdays = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
     var maleName =["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
     var femaleName = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
+
+    var date = document.getElementById("date");
+    var date = new Date(document.getElementById("date").value);
+    var dateday = date.getDay();
 }
